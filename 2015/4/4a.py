d9a4439795503d1ae50db7be70db65cb72260962
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-01-05 09:36:49 +0000

    add 2015 day 4 (late upload)

---
+++ 2015/4/4a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import hashlib
+
+n = 0
+found = False
+fileHandle = open("4.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+secretKey = fileData.strip()
+while found == False:
+	n += 1
+	x = secretKey + str(n)
+	nHash = hashlib.md5(x.encode("utf-8")).hexdigest()
+	if nHash[:6] == '000000':
+		found = True
+		print(n)
