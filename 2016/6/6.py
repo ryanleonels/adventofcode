e2ab455ce0fdf1a6623bfd3452acc147dacccff0
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-02-06 15:22:32 +0000

    add 2016 day 6

---
+++ 2016/6/6.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+message = ""
+columns = []
+fileHandle = open("6.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileLines = fileData.split('\n')
+n = 0
+for fileLine in fileLines:
+	if fileLine.strip() == '':
+		continue
+	if n == 0:
+		n = len(fileLine)
+		for i in range(0, n):
+			columns.append({})
+	for i in range(0, n):
+		ch = fileLine[i]
+		if ch in columns[i]:
+			columns[i][ch] += 1
+		else:
+			columns[i][ch] = 1
+for i in range(0, n):
+	ch1 = ''
+	maxN = 0
+	for ch in columns[i]:
+		curN = columns[i][ch]
+		if curN > maxN:
+			maxN = curN
+			ch1 = ch
+	message += ch1
+print(message)
