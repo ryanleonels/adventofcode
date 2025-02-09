9ccad447bc3b4804aac9eea661ad93d6df709f31
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-02-09 09:34:36 +0000

    add 2016 day 9

---
+++ 2016/9/9a.py
@@ -0,0 +1,46 @@
+#!/usr/local/bin/python3
+
+fileHandle = open("9.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+data = fileData.strip()
+
+def decompress(dataStr):
+	result = 0
+	n = len(dataStr)
+	pos = 0
+	markerState = 0
+	(seqLength, seqTimes, seqStart) = (0, 0, 0)
+	curNum = ""
+	sequence = ""
+	while pos < n:
+		ch = dataStr[pos]
+		match markerState:
+			case 0: # not in sequence
+				if ch == '(':
+					markerState = 1
+					curNum = ""
+				else:
+					result += 1
+			case 1: # sequence length
+				if ch == 'x':
+					markerState = 2
+					seqLength = int(curNum)
+					curNum = ""
+				else:
+					curNum += ch
+			case 2: # sequence times
+				if ch == ')':
+					markerState = 0
+					seqTimes = int(curNum)
+					seqStart = pos + 1
+					sequence = dataStr[seqStart:seqStart+seqLength]
+					result += (decompress(sequence) * seqTimes)
+					pos += seqLength
+				else:
+					curNum += ch
+		pos += 1
+	return result
+
+output = decompress(data)
+print(output)
