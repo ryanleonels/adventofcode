959a818e235ab8ba601378ef93f997a24c8356e4
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-02-16 10:27:34 +0000

    add 2016 day 15

---
+++ 2016/15/15a.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+fileHandle = open("15.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileLines = fileData.split('\n')
+discs = []
+for fileLine in fileLines:
+	if fileLine.strip() == '':
+		continue
+	numpos = int(fileLine.split(' has ')[1].split(' positions')[0])
+	startpos = int(fileLine.split(' at position ')[1].split('.')[0])
+	discs.append((numpos, startpos))
+discs.append((11, 0))
+n = len(discs)
+#print(n)
+#print(discs)
+mods = []
+for i in range(0, n):
+	(numpos, startpos) = discs[i]
+	pos0 = (startpos + (i + 1)) % numpos
+	goalmod = (numpos - pos0) % numpos
+	#print(str(goalmod) + " mod " + str(numpos))
+	mods.append((goalmod, numpos))
+t = 0
+done = False
+while not done:
+	done = True
+	for i in range(0, n):
+		if t % mods[i][1] != mods[i][0]:
+			done = False
+			break
+	if done:
+		print(t)
+	else:
+		t += 1
