a40622bc0df1739698c49db5719c387998372d1a
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-03-09 09:19:41 +0000

    add 2017 day 9

---
+++ 2017/9/9a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+fileHandle = open("9.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileStream = fileData.strip()
+
+def getScore(stream):
+	n = len(stream)
+	pos = 0
+	level = 0
+	score = 0
+	garbageCount = 0
+	inGarbage = False
+	isCanceled = False
+	while pos < n:
+		if isCanceled == True:
+			isCanceled = False
+		else:
+			ch = stream[pos]
+			if inGarbage == True:
+				if ch == '!':
+					isCanceled = True
+				if ch == '>':
+					inGarbage = False
+				if ch != '!' and ch != '>':
+					garbageCount += 1
+			else:
+				if ch == '<':
+					inGarbage = True
+				if ch == '{':
+					level += 1
+				if ch == '}':
+					score += level
+					level -= 1
+				if ch == ',':
+					pass
+		pos += 1
+	return garbageCount
+
+print(getScore(fileStream))
