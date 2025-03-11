338e80400a1421957aa51ff9dd4308d8e1cbbd98
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-03-11 13:01:01 +0000

    add 2017 day 11

---
+++ 2017/11/11.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+fileHandle = open("11.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+steps = fileData.strip().split(',')
+n = len(steps)
+
+(x, y) = (0, 0)
+for i in range(0, n):
+	step = steps[i]
+	if step == "n":
+		(x, y) = (x, y + 2)
+	if step == "nw":
+		(x, y) = (x - 1, y + 1)
+	if step == "ne":
+		(x, y) = (x + 1, y + 1)
+	if step == "s":
+		(x, y) = (x, y - 2)
+	if step == "sw":
+		(x, y) = (x - 1, y - 1)
+	if step == "se":
+		(x, y) = (x + 1, y - 1)
+
+#print((x, y))
+dist = 0
+(x1, y1) = (abs(x), abs(y))
+if x1 >= y1:
+	dist = x1
+else:
+	dist = x1 + ((y1 - x1) // 2)
+print(dist)
