effa65752e5fe4f39e71d6fb9af591c541c196c9
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-03-12 17:37:10 +0000

    add 2017 day 12

---
+++ 2017/12/12.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+from heapq import heapify, heappop, heappush
+
+fileHandle = open("12.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileLines = fileData.split('\n')
+
+adjs = {}
+for fileLine in fileLines:
+	if fileLine.strip() == "":
+		continue
+	a = int(fileLine.split(' <-> ')[0])
+	b = [int(x) for x in fileLine.split(' <-> ')[1].split(', ')]
+	adjs[a] = b
+
+adj = {}
+for a in adjs:
+	adj[a] = {}
+	for b in adjs[a]:
+		if a != b:
+			adj[a][b] = 1
+
+class Graph:
+	def __init__(self, graph: dict = {}):
+		self.graph = graph  # A dictionary for the adjacency list
+
+	def add_edge(self, node1, node2, weight):
+		if node1 not in self.graph:  # Check if the node is already added
+			self.graph[node1] = {}  # If not, create the node
+		self.graph[node1][node2] = weight  # Else, add a connection to its neighbor
+
+	def shortest_distances(self, source: str):
+		global end, minScore
+
+		# Initialize the values of all nodes with infinity
+		distances = {node: float("inf") for node in self.graph}
+		distances[source] = 0  # Set the source value to 0
+
+		# Initialize a priority queue
+		pq = [(0, source)]
+		heapify(pq)
+
+		# Create a set to hold visited nodes
+		visited = set()
+
+		while pq:  # While the priority queue isn't empty
+			current_distance, current_node = heappop(pq) # Get the node with the min distance
+
+			if current_node in visited:
+			   continue  # Skip already visited nodes
+			visited.add(current_node)  # Else, add the node to visited set
+
+			for neighbor, weight in self.graph[current_node].items():
+				# Calculate the distance from current_node to the neighbor
+				tentative_distance = current_distance + weight
+				if tentative_distance < distances[neighbor]:
+					distances[neighbor] = tentative_distance
+					heappush(pq, (tentative_distance, neighbor))
+
+		return distances
+
+adjGraph = Graph(adj)
+distances = adjGraph.shortest_distances(0)
+n = len(adjs)
+programs = 0
+for a in distances:
+	if distances[a] <= n:
+		programs += 1
+print(programs)
