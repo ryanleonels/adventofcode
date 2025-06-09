bd029ed217eb96291f2a664d3382416860476776
Ryan Leonel Somali <ryanleonelsomali@Ryans-MacBook-Pro.local>
2025-06-09 14:59:37 +0100

    add 2020 day 9

---
+++ 2020/9/9a.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+nums = []
+fileHandle = open("9.in", "r")
+fileData = fileHandle.read()
+fileHandle.close()
+fileLines = fileData.split('\n')
+
+for fileLine in fileLines:
+	if fileLine.strip() == '':
+		continue
+	num = int(fileLine.strip())
+	nums.append(num)
+
+n = len(nums)
+preambleSize = 25
+invalidIndex = -1
+for i in range(preambleSize, n):
+	sum2 = False
+	for j in range(i - preambleSize, i):
+		for k in range(j, i):
+			if nums[j] + nums[k] == nums[i]:
+				sum2 = True
+	if not sum2:
+		invalidIndex = i
+		break
+
+for i in range(0, invalidIndex):
+	sum1 = 0
+	for j in range(i, invalidIndex):
+		sum1 += nums[j]
+		if sum1 >= nums[invalidIndex]:
+			break
+	if sum1 == nums[invalidIndex]:
+		print(min(nums[i:j+1]) + max(nums[i:j+1]))
+		break
